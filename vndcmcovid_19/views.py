0ef7156ace473c0135366f6601b59e9f32b63142
hung <hungamtp@gmail.com>
2020-08-20 21:44:54 +0700

    done

--- vndcmcovid_19/views.py
+++ vndcmcovid_19/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render   , HttpResponse
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from unidecode import unidecode
+
 import time
 from selenium import webdriver
 # Create your views here.
 def index(request):
-    
     PATH ='/home/khunglongbaochua/Desktop/chromedriver'
     driver = webdriver.Chrome(PATH)
-    driver.get('https://ncov.moh.gov.vn/?fbclid=IwAR0WTvaWHDTbf52Ojb9OmsHMJdt2QCl1TXMYZyJh90vwYs2NcS8K7utmwt0')
+    driver.get('https://ncov.moh.gov.vn/')
     try:
         provinces = driver.find_element_by_tag_name('tbody').text
     except:
@@ -29,20 +27,20 @@ def index(request):
         word = line.split(' ')
         word1 = line.split(' ')
         if word1[2].isnumeric():
-            table[word1[0] + ' ' +word1[1]] = [word1[2] , word1[3] , word1[4] , word[5]]
+            table[ unidecode(word1[0]) + '_' +unidecode(word1[1])] = [word1[2] , word1[3] , word1[4] , word[5]]
             case += int(word1[2])
             treating += int(word1[3])
             recovered   += int(word1[4])          
             deaths += int(word1[5])
         elif word1[3].isnumeric() and not word1[2].isnumeric():
-            table[word1[0] + ' ' +word1[1] + ' ' + word1[2]] = [word1[3] , word1[4] , word1[5] , word[6]]       
+            table[ unidecode(word1[0]) + '_' +unidecode(word1[1]) + '_' + unidecode(word1[2]) ] = [word1[3] , word1[4] , word1[5] , word[6] ]       
             case += int(word1[3])
             treating += int(word1[4])
             recovered   += int(word1[5])          
             deaths += int(word1[6])
         else:
 
-            table[word1[0] + ' ' +word1[1] + ' ' + word1[2] +' ' +word1[3]] = [ word1[4] , word1[5] , word1[6] , word[7] ]
+            table[ unidecode(word1[0]) + '_' +unidecode(word1[1]) + '_' + unidecode(word1[2]) +'_' +unidecode(word1[3]) ] = [ word1[4] , word1[5] , word1[6] , word[7] ]
             case += int(word1[4])
             treating += int(word1[5])
             recovered   += int(word1[6])          
@@ -60,9 +58,8 @@ def index(request):
 
 def test(request):
     d = {'one':'itemone', 'two':'itemtwo', 'three':'itemthree'}
-    a = 5
+
     context={
         'd': d,
-        'a':a
     }
     return render(request  , 'test.html' , context)
